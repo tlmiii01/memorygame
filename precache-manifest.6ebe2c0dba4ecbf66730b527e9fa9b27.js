b0e555a6e1cd6364efb3f95dfdc8c19885b8f736
Thomas Martin <tmartin.dev101010@gmail.com>
2019-01-20 18:49:54 -0700

    Updates

---
+++ precache-manifest.6ebe2c0dba4ecbf66730b527e9fa9b27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "2557a041b7513b3d1aae",
+    "url": "/memorygame/static/js/main.2557a041.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/memorygame/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "2557a041b7513b3d1aae",
+    "url": "/memorygame/static/css/main.962c732e.chunk.css"
+  },
+  {
+    "revision": "593cb245daabe113b97c53f4433ae990",
+    "url": "/memorygame/index.html"
+  }
+];
